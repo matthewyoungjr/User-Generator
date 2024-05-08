f4769db893983510a612a37eff7973820299816e
matthewyoungjr <hawkeyeanon@protonmail.com>
2024-05-08 17:34:12 +0000

    User info component

---
+++ src/components/Infos.tsx
@@ -0,0 +1,14 @@
+const Infos = () => {
+  return (
+    <div>
+      <h2 className="text-xl">Name : John Doe</h2>
+      <p className="text-xl">Email : user@gmail.com</p>
+      <p className="text-xl">Phone : 00000000</p>
+      <p className="text-xl">Location : Abidjan</p>
+      <p className="text-xl">Age : Number</p>
+      <p className="text-xl">Gender : Female</p>
+    </div>
+  );
+};
+
+export default Infos;
