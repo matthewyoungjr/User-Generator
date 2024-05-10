052bfb338fde97c2e317c07ff727277ca3183e14
matthewyoungjr <hawkeyeanon@protonmail.com>
2024-05-10 10:38:29 +0000

    thank you JESUS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.css";
+import Card from "./components/Card";
+import Title from "./components/Title";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="mb-5 text-2xl">
+        <Title />
+      </div>
+
+      <Card />
     </>
   );
 }
